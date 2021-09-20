32288916590dfb36343e9d3c48aac73f5a5110d8
gabriellysoares28 <gabriellysoares279@gmail.com>
2021-09-20 16:37:47 -0300

    criação dos repositorios

--- backend/src/main/java/weeklyBasis/repositorio/EventoRepositorio.java
+++ backend/src/main/java/weeklyBasis/repositorio/EventoRepositorio.java
@@ -1,6 +1,7 @@
 package weeklyBasis.repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import weeklyBasis.dominios.Evento;
 
 @Repository
 public interface EventoRepositorio extends JpaRepository<Evento, Long> {
--- backend/src/main/java/weeklyBasis/repositorio/UsuarioRepositorio.java
+++ backend/src/main/java/weeklyBasis/repositorio/UsuarioRepositorio.java
@@ -2,6 +2,7 @@ package weeklyBasis.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import weeklyBasis.dominios.Usuario;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
