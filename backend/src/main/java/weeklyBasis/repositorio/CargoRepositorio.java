8da1bad6fe87d17d25b8396eef44f93b659ad22b
edson <edsonmaatheus@gmail.com>
2021-10-04 17:25:07 -0300

    Ajustando

--- backend/src/main/java/weeklyBasis/repositorio/CargoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package weeklyBasis.repositorio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import weeklyBasis.dominios.Cargo;
-
-@Repository
-public interface CargoRepositorio extends JpaRepository<Cargo, Long> {
-}
--- backend/src/main/java/weeklyBasis/repositorio/EventoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package weeklyBasis.repositorio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import weeklyBasis.dominios.Evento;
-
-@Repository
-public interface EventoRepositorio extends JpaRepository<Evento, Long> {
-
-}
